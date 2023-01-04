ded360c52b853226ea845572e68adcefcdfa772e
Davi Silva <davisilvaphoto@gmail.com>
2023-01-04 09:17:58 -0300

    feat: image upload

--- pages/user/publish.tsx
+++ pages/user/publish.tsx
@@ -1,14 +1,37 @@
+import { DeleteForever } from '@mui/icons-material';
 import {
   Box,
+  Button,
   Container,
+  IconButton,
   Select,
   TextField,
   Typography,
-  Button,
 } from '@mui/material';
 import { TemplateDefault } from '../../src/templates/TemplateDefault';
 
+import { useCallback, useEffect, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+
 export default function PublishedPage() {
+  const [files, setFiles] = useState<File[]>([]);
+
+  const onDrop = useCallback(
+    (acceptedFiles: File[]) => {
+      const newFiles = acceptedFiles.map((file) => URL.createObjectURL(file));
+
+      setFiles((prev) => [...prev, newFiles]);
+    },
+    [setFiles]
+  );
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+  });
+
+  const handleRemoveFile = (file: File) =>
+    setFiles(files.filter((fileSaved) => fileSaved !== file));
+
   return (
     <TemplateDefault>
       <Container
@@ -55,7 +78,8 @@ export default function PublishedPage() {
             onChange={() => {}}
             inputProps={{}}
           >
-            <option value="">Selecione</option>
+            <option value="">Selecione a categoria</option>
+            <option value={1}></option>
             <option value={1}>Bebê e Criança</option>
             <option value={2}>Agricultura</option>
             <option value={3}>Moda</option>
@@ -81,9 +105,89 @@ export default function PublishedPage() {
             Imagens
           </Typography>
 
-          <Typography component={'div'} variant="body2">
+          <Typography component={'div'} variant="body2" gutterBottom>
             A primeira imagem é a principal
           </Typography>
+
+          <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+            <Box
+              {...getRootProps()}
+              sx={{
+                padding: 1,
+                width: 200,
+                height: 150,
+                backgroundColor: 'rgb(242, 244, 245)',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                border: 'dashed',
+                textAlign: 'center',
+                cursor: 'pointer',
+                margin: '0 15px 15px 0',
+              }}
+            >
+              <input {...getInputProps()} />
+
+              <Typography variant="body2">
+                Clique para adicionar ou arraste a imagem aqui
+              </Typography>
+            </Box>
+
+            {files.map((file, index) => {
+              return (
+                <Box
+                  key={index}
+                  sx={{
+                    position: 'relative',
+                    width: 200,
+                    height: 150,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center center',
+                    margin: '0 15px 15px 0',
+                  }}
+                  style={{
+                    backgroundImage: `url(${file})`,
+                  }}
+                >
+                  {index === 0 ? (
+                    <Box
+                      sx={{
+                        backgroundColor: 'blue',
+                        padding: '6px 10px',
+                        position: 'absolute',
+                        color: '#fff',
+                        textTransform: 'uppercase',
+                        bottom: 0,
+                        left: 0,
+                      }}
+                    >
+                      <Typography variant="body2">Principal</Typography>
+                    </Box>
+                  ) : null}
+
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      textAlign: 'center',
+                      width: '100%',
+                      height: '100%',
+
+                      '&:hover': {
+                        backgroundColor: 'rgba(0,0,0,0.4)',
+                        cursor: 'pointer',
+                      },
+                    }}
+                  >
+                    <IconButton onClick={() => handleRemoveFile(file)}>
+                      <DeleteForever sx={{ color: '#fff' }} fontSize="large" />
+                    </IconButton>
+                  </Box>
+                </Box>
+              );
+            })}
+          </Box>
         </Box>
       </Container>
 
@@ -124,6 +228,7 @@ export default function PublishedPage() {
           />
 
           <TextField size="small" label="E-mail" variant="outlined" fullWidth />
+
           <TextField
             size="small"
             label="Telefone"
